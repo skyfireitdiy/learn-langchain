0b9034658b638b95acdd2a46e38d1fc1ff8668a0
skyfire <skyfireitdiy@hotmail.com>
2024-03-03 10:33:21 +0800

    提前安装包

---
+++ 4.install_package.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip install torch tabulate tqdm transformers accelerate sentencepiece chatglm.cpp uvicorn 'chatglm-cpp[api]'
--- 6.install_chatglm.sh
+++
@@ -1,4 +0,0 @@
-cd 3rd/chatglm.cpp
-pip install .
-pip install uvicorn 'chatglm-cpp[api]'
-cp -v build/lib/linux-x86_64-cpython-312/chatglm_cpp/_C.cpython-312-x86_64-linux-gnu.so ./chatglm_cpp/
